ca5721ae3727ab77d8968e643cb29938f6fc12a7
Leo Mehlig <leo@leomehlig.com>
2018-04-27 12:46:55 +0200

    pull

--- fastlane/push.sh
+++ fastlane/push.sh
@@ -6,13 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b master
+  git checkout master
+  git pull
   git commit -m CI\ commit\ \[ci\ skip\] ./docs/\* ./TDLib/Info.plist ./TDLibTests/Info.plist ./TDLib.xcodeproj/project.pbxproj
 }
 
 upload_files() {
-  git remote add origin git@github.com:leoMehlig/TDLib-iOS.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
+  git remote add origin-master git@github.com:leoMehlig/TDLib-iOS.git > /dev/null 2>&1
+  git push origin-master master
 }
 cd ..
 setup_git
